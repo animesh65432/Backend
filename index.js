c012d61277a02afcf9240490054d6363d9a2d3e6
animesh65432 <kiranduttta234@gmail.com>
2024-03-28 09:00:44 +0530

    Create the /add-product

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 import express from "express";
+import bodyparser from "body-parser";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello to Node js");
-});
+app.use(bodyparser.urlencoded({ extended: false }));
+
+const add_productrouter = (req, res) => {
+  return res.send(
+    "<html><form action='/product' method='post'><input name='username' placeholder='please put your name here'></input><button>Add</button></form></html>"
+  );
+};
+
+app.get("/add_product", add_productrouter);
+
+const showtheproduct = (req, res) => {
+  console.log(req.body);
+  res.send("Product added successfully!");
+};
+
+app.post("/product", showtheproduct);
 
 app.listen(3000, () => {
-  console.log("You server at listening at the 3000");
+  console.log("Your server is listening at port 3000");
 });
